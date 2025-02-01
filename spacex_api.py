3455982dcebfa961c9aa562845c33a0d7c7f68eb
Martin Bailey <martin@nglaunchpad.com>
2025-02-01 11:00:17 +0000

    added spacex_api.py

---
+++ spacex_api.py
@@ -0,0 +1,11 @@
+
+import pip._vendor.requests as requests
+
+import pandas as pd
+
+response = requests.get('https://api.spacexdata.com/v4/launches')
+data = response.json()
+
+df = pd.DataFrame(data)
+
+print(df.head())
